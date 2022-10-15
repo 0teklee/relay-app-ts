1c1c0d48a577e485742a1361104decca83e8fb0e
0teklee <leetekwoo@gmail.com>
2022-10-15 15:17:43 +0900

    feat: added search query aritifact

---
+++ libs/relay/__generated__/pages_index_search_Query.graphql.ts
@@ -0,0 +1,260 @@
+/**
+ * @generated SignedSource<<49aef37aa656773a2e52da8f3cc79840>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Query } from 'relay-runtime';
+export type SearchType = "DISCUSSION" | "ISSUE" | "REPOSITORY" | "USER" | "%future added value";
+export type pages_index_search_Query$variables = {
+  first?: number | null;
+  query: string;
+  type: SearchType;
+};
+export type pages_index_search_Query$data = {
+  readonly search: {
+    readonly nodes: ReadonlyArray<{
+      readonly createdAt?: any;
+      readonly description?: string | null;
+      readonly name?: string;
+      readonly stargazers?: {
+        readonly totalCount: number;
+      };
+      readonly watchers?: {
+        readonly totalCount: number;
+      };
+    } | null> | null;
+    readonly pageInfo: {
+      readonly endCursor: string | null;
+      readonly hasNextPage: boolean;
+    };
+  };
+};
+export type pages_index_search_Query = {
+  response: pages_index_search_Query$data;
+  variables: pages_index_search_Query$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "first"
+  },
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "query"
+  },
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "type"
+  }
+],
+v1 = [
+  {
+    "kind": "Variable",
+    "name": "first",
+    "variableName": "first"
+  },
+  {
+    "kind": "Variable",
+    "name": "query",
+    "variableName": "query"
+  },
+  {
+    "kind": "Variable",
+    "name": "type",
+    "variableName": "type"
+  }
+],
+v2 = {
+  "alias": null,
+  "args": null,
+  "concreteType": "PageInfo",
+  "kind": "LinkedField",
+  "name": "pageInfo",
+  "plural": false,
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "hasNextPage",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "endCursor",
+      "storageKey": null
+    }
+  ],
+  "storageKey": null
+},
+v3 = [
+  {
+    "alias": null,
+    "args": null,
+    "kind": "ScalarField",
+    "name": "totalCount",
+    "storageKey": null
+  }
+],
+v4 = {
+  "kind": "InlineFragment",
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "name",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "createdAt",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "description",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": "UserConnection",
+      "kind": "LinkedField",
+      "name": "watchers",
+      "plural": false,
+      "selections": (v3/*: any*/),
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": "StargazerConnection",
+      "kind": "LinkedField",
+      "name": "stargazers",
+      "plural": false,
+      "selections": (v3/*: any*/),
+      "storageKey": null
+    }
+  ],
+  "type": "Repository",
+  "abstractKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "pages_index_search_Query",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": "SearchResultItemConnection",
+        "kind": "LinkedField",
+        "name": "search",
+        "plural": false,
+        "selections": [
+          (v2/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": null,
+            "kind": "LinkedField",
+            "name": "nodes",
+            "plural": true,
+            "selections": [
+              (v4/*: any*/)
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "pages_index_search_Query",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": "SearchResultItemConnection",
+        "kind": "LinkedField",
+        "name": "search",
+        "plural": false,
+        "selections": [
+          (v2/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": null,
+            "kind": "LinkedField",
+            "name": "nodes",
+            "plural": true,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "__typename",
+                "storageKey": null
+              },
+              (v4/*: any*/),
+              {
+                "kind": "InlineFragment",
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "id",
+                    "storageKey": null
+                  }
+                ],
+                "type": "Node",
+                "abstractKey": "__isNode"
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "6da4ecb6ab1321b4cd3e90f982880066",
+    "id": null,
+    "metadata": {},
+    "name": "pages_index_search_Query",
+    "operationKind": "query",
+    "text": "query pages_index_search_Query(\n  $first: Int\n  $query: String!\n  $type: SearchType!\n) {\n  search(first: $first, query: $query, type: $type) {\n    pageInfo {\n      hasNextPage\n      endCursor\n    }\n    nodes {\n      __typename\n      ... on Repository {\n        name\n        createdAt\n        description\n        watchers {\n          totalCount\n        }\n        stargazers {\n          totalCount\n        }\n      }\n      ... on Node {\n        __isNode: __typename\n        id\n      }\n    }\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "c0fcee96be643e15e504abf6a563f7ef";
+
+export default node;
