5ff624229721f10813ead1b2986cd277d71e92d6
0teklee <leetekwoo@gmail.com>
2022-10-15 15:17:01 +0900

    fix: init relay query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,72 @@
-import type { NextPage } from "next";
+import { fetchQuery, graphql } from "react-relay";
+import { initEnvironment } from "libs/relay/relayEnvironment";
 
-const Home: NextPage = () => {
-  return <></>;
+import Title from "components/common/Title";
+import Layout from "components/common/Layout";
+import SearchBar from "components/module/SearchBar";
+
+import { useState } from "react";
+
+export default function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <div className="w-screen h-full p-12 bg-red-600">
+      {false ? (
+        "Loading..."
+      ) : (
+        <>
+          <Layout>
+            <Title text="Github Search" />
+            <Title subTitle text="built with Relay & Next.js" />
+            <SearchBar setState={setSearchQuery} />
+          </Layout>
+        </>
+      )}
+    </div>
+  );
+}
+
+const query = graphql`
+  query pages_index_search_Query(
+    $first: Int
+    $query: String!
+    $type: SearchType!
+  ) {
+    search(first: $first, query: $query, type: $type) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      nodes {
+        ... on Repository {
+          name
+          createdAt
+          description
+          watchers {
+            totalCount
+          }
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const getStaticProps = async () => {
+  const environment = initEnvironment();
+  try {
+    const initialRecords = environment.getStore().getSource().toJSON();
+
+    return {
+      props: {
+        initialRecords,
+      },
+    };
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
-
-export default Home;
