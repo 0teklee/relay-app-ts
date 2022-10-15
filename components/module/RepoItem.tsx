f0b439079f2647e68813e8a3d461e678c2b76c37
0teklee <leetekwoo@gmail.com>
2022-10-15 22:37:02 +0900

    fix: fixed result item query to edge for pagination

--- components/module/RepoItem.tsx
+++ components/module/RepoItem.tsx
@@ -1,33 +1,40 @@
-import { graphql, useFragment } from "react-relay/hooks";
+import { graphql, useFragment, usePaginationFragment } from "relay-hooks";
 import type { RepoItem_Repository$key } from "libs/relay/__generated__/RepoItem_Repository.graphql";
 
+// RepoItem Fragment & index before relay compile
 const fragment = graphql`
-  fragment RepoItem_Repository on SearchResultItem {
-    ... on Repository {
-      name
-      createdAt
-      description
-      watchers {
-        totalCount
-      }
-      stargazers {
-        totalCount
+  fragment RepoItem_Repository on SearchResultItemEdge {
+    cursor
+    node {
+      ... on SearchResultItem {
+        ... on Repository {
+          databaseId
+          name
+          createdAt
+          description
+          watchers {
+            totalCount
+          }
+          stargazers {
+            totalCount
+          }
+        }
       }
     }
   }
 `;
 
 interface IProps {
-  node: RepoItem_Repository$key;
+  edge: RepoItem_Repository$key;
 }
 
-const RepoItem_Repository = ({ node }: IProps) => {
-  const data = useFragment(fragment, node);
-  const { name, createdAt, description = "", stargazers, watchers } = data;
+const RepoItem_Repository = ({ edge }: IProps) => {
+  const { node, cursor } = useFragment(fragment, edge);
+  const { name, createdAt, description = "", stargazers, watchers } = node;
   const LocaleDate = new Date(createdAt).toLocaleString("ko-KR", {
     dateStyle: "medium",
   });
-  console.log("innerItem", data);
+  console.log("child frag cursor", cursor);
   return (
     <div className="mb-8 p-5 border-b border-gray-300">
       <h3 className="mb-4 text-2xl font-bold">{name}</h3>
