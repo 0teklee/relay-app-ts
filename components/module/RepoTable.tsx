742f54acfc1309ace4f5a363ad2fd994fea6a8f9
0teklee <leetekwoo@gmail.com>
2022-10-16 04:11:08 +0900

    chore:  minor style change removed result title

--- components/module/RepoTable.tsx
+++ components/module/RepoTable.tsx
@@ -1,12 +1,7 @@
 import { ReactNode } from "react";
 
 const RepoTable = ({ children }: { children: ReactNode }) => {
-  return (
-    <>
-      <h3 className="mb-6 text-2xl font-bold">Results</h3>
-      <div className="p-8 w-full h-full bg-blue-300">{children}</div>
-    </>
-  );
+  return <div className="p-8 w-full h-full bg-blue-300">{children}</div>;
 };
 
 export default RepoTable;
