1bbdf909d79ee5031c37b463d97a2b5a8d901f84
0teklee <leetekwoo@gmail.com>
2022-10-15 14:49:39 +0900

    chore: initial settings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+const Dotenv = require("dotenv-webpack");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config) => {
+    config.plugins.push(new Dotenv({ silent: true }));
+    config.resolve.preferRelative = true;
+    return config;
+  },
+  compiler: {
+    relay: {
+      src: "./",
+      artifactDirectory: "libs/relay/__generated__",
+      language: "typescript",
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ scripts/generateSchema.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+const fetch = require("isomorphic-fetch");
+const {
+  getIntrospectionQuery,
+  buildClientSchema,
+  printSchema,
+} = require("graphql");
+
+const generateSchema = async () => {
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+  try {
+    if (GITHUB_TOKEN) {
+      const response = await fetch("https://api.github.com/graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `bearer ${GITHUB_TOKEN}`,
+        },
+        body: JSON.stringify({
+          query: getIntrospectionQuery(),
+        }),
+      });
+      const res = await response.json();
+      const sdl = printSchema(buildClientSchema(res.data));
+      const parentPath = path.join(__dirname, "../");
+      fs.writeFileSync(`${parentPath}/schema.graphql`, sdl);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+generateSchema();
