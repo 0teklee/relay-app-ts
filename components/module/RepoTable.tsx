07c689f7bf95215b909f15f501193b6203591a46
0teklee <leetekwoo@gmail.com>
2022-10-16 16:25:01 +0900

    chore: minor style changes

--- components/module/RepoTable.tsx
+++ components/module/RepoTable.tsx
@@ -1,7 +1,11 @@
 import { ReactNode } from "react";
 
 const RepoTable = ({ children }: { children: ReactNode }) => {
-  return <div className="p-8 w-full h-full bg-blue-300">{children}</div>;
+  return (
+    <div className="p-8 rounded-2xl border border-blue-400 bg-white w-full h-full max-w-3xl bg-blue-300">
+      {children}
+    </div>
+  );
 };
 
 export default RepoTable;
