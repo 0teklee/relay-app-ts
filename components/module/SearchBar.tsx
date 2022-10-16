dca116877093e2c023686fe7b43001e1bc4445b6
0teklee <leetekwoo@gmail.com>
2022-10-16 16:15:36 +0900

    chore: minor style changes

--- components/module/SearchBar.tsx
+++ components/module/SearchBar.tsx
@@ -11,14 +11,14 @@ const SearchBar = ({
       <form>
         <input
           type="text"
-          className="w-72 pt-3 pb-3 pl-6 pr-2 rounded-l-xl"
+          className="w-72 pt-3 pb-3 pl-6 pr-2 rounded-l-xl border-t border-b border-l border-grey-700 outline-none focus-within:border-blue-400"
           onChange={(e) => {
             textRef.current = e.target.value;
           }}
         />
         <button
           type="submit"
-          className="p-3 rounded-r-xl bg-blue-400 text-white"
+          className="p-3 rounded-r-xl border-t border-b border-r border-blue-400 bg-blue-400 text-white"
           onClick={(e) => {
             e.preventDefault();
             setState(textRef.current);
