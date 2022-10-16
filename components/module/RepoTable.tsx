9c813b7eabc051511696ea5bedab778b192bfea3
0teklee <leetekwoo@gmail.com>
2022-10-17 01:44:08 +0900

    chore: fixed bg color and minor style changes

--- components/module/RepoTable.tsx
+++ components/module/RepoTable.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const RepoTable = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="p-8 rounded-2xl border border-blue-400 bg-white w-full h-full max-w-3xl bg-blue-300">
+    <div className="w-full h-full max-w-3xl p-8 rounded-2xl border border-gray-300 bg-white">
       {children}
     </div>
   );
