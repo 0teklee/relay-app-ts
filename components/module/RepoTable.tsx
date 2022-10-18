d7ccd49bdc45d9000b08881b672b46b8bc06c931
0teklee <leetekwoo@gmail.com>
2022-10-18 13:08:34 +0900

    chore: add position relative for spinner, minor style change

--- components/module/RepoTable.tsx
+++ components/module/RepoTable.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const RepoTable = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="w-full h-full max-w-3xl p-8 rounded-2xl border border-gray-300 bg-white">
+    <div className="relative w-full h-full max-w-3xl p-8 rounded-2xl border border-gray-300 bg-white">
       {children}
     </div>
   );
