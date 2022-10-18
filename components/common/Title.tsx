e1e1dccc24b2e4497195c4c54c7df16a6618b79f
0teklee <leetekwoo@gmail.com>
2022-10-18 13:03:31 +0900

    refactor: added author branch and props for it

--- components/common/Title.tsx
+++ components/common/Title.tsx
@@ -1,6 +1,18 @@
-const Title = ({ text, subTitle }: { text: string; subTitle?: boolean }) => {
+const Title = ({
+  text,
+  subTitle,
+  author,
+}: {
+  text: string;
+  subTitle?: boolean;
+  author?: boolean;
+}) => {
+  if (author) {
+    return <h3 className="mb-12 text-2xl font-bold">{text}</h3>;
+  }
+
   if (subTitle) {
-    return <h2 className="mb-12 text-3xl font-bold">{text}</h2>;
+    return <h2 className="mb-4 text-3xl font-bold">{text}</h2>;
   }
   return <h1 className="mb-6 text-5xl font-bold">{text}</h1>;
 };
